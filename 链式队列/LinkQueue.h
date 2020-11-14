9fb3bb2122f1d4955b8b1996d4d82d9a3c0a4050
lph <liupeter012001@live.cn>
2020-11-14 10:52:38 +0800

    添加项目文件。

---
+++ 链式队列/LinkQueue.h
@@ -0,0 +1,40 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<Windows.h>
+#include<assert.h>
+
+
+#define ElemType int
+
+typedef struct LinkNode       //链表
+{
+          ElemType data;      //数据域
+          struct LinkNode* next;  //指针域
+}LinkNode;
+
+typedef struct LinkQueue      //链式队列
+{
+          LinkNode* front;    //头指针
+          LinkNode* rear;     //尾指针
+}LinkQueue;
+
+/*链式队列的初始化摧毁与清除功能*/
+void InitLinkQueue(LinkQueue* Q);           //链式队列的初始化
+void ClearLinkQueue(LinkQueue* Q);           //链式队列的清除
+void DestroyLinkQueue(LinkQueue* Q);           //链式队列的摧毁
+
+/*链式队列的显示输出*/
+void DisplayLinkQueue(LinkQueue* Q);           //链式队列的输出
+
+/*判空以及判满操作*/
+BOOL IsQueueEmpty(LinkQueue Q);              //链式队列的判空
+BOOL IsQueueFull(LinkQueue Q);              //链式队列的判满
+
+/*入队以及出队操作*/
+BOOL EnQueue(LinkQueue* Q, ElemType x);            //链式队列的入队操作
+BOOL DeQueue(LinkQueue* Q, ElemType* e);             //链式队列的出队操作
+
+/*其他功能*/
+ElemType GetHead(LinkQueue Q);              //链式队列获取队头的值
+int length(LinkQueue Q);                 //链式队列长度
---
+++ 链式队列/main.c
@@ -0,0 +1,7 @@
+#include"LinkQueue.h"
+
+int main()
+{
+
+          return 0;
+}
